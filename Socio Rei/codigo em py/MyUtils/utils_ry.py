8bce0bfa7b4d5504c1b06dbc23006f901c8a5d88
Ryan Silva <ryanprogrammerpy@gmail.com>
2022-03-11 16:41:23 -0300

    Att chromedriver and update code

--- Socio Rei/codigo em py/MyUtils/utils_ry.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def checkNet():
-    try:
-        requests.get('https://www.google.com/')
-    except requests.exceptions.ConnectionError:
-        return False
-    else:
-        return True
