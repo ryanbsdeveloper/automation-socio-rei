50e9c181bf4f3969e140ed68e407f6ac723127cb
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-07-07 04:02:39 -0300

    Update bot

--- Socio Rei/.exe e código/socio_rei.py
+++ Socio Rei/.exe e código/socio_rei.py
@@ -2,34 +2,35 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from MyUtils.utils_ry import checkNet
+from myutils.utils_ry import checkNet
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class SocioRei:
     def __init__(self):
-        chrome_service = Service(executable_path='chromedriver.exe')
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument(r'--incognito')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        self.chrome = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        # options.add_argument(r'--incognito')
+        # options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.chrome = webdriver.Chrome(service=service, options=options)
 
     def open_site(self, site):
-        sleep(3)
+        sleep(2)
         self.chrome.get(site)
 
     def enter_account(self):
-        sleep(10)
-        botao_entrar = self.chrome.find_element(By.XPATH, '/html/body/app-root/div/app-header/header/div/div/div/a')
+        sleep(2)
+        botao_entrar = self.chrome.find_element(By.XPATH, '/html/body/app-root/div/fengstlayout-header/header/section/nav/a[1]')
         botao_entrar.click()
 
-        sleep(7)
+        sleep(2)
 
-        email = self.chrome.find_element(By.XPATH, '/html/body/div/div[2]/div/mat-dialog-container/app-modal-auth/div/mat-dialog-content/div[2]/form/mat-form-field[1]/div/div[1]/div/input')
-        senha = self.chrome.find_element(By.XPATH, '/html/body/div/div[2]/div/mat-dialog-container/app-modal-auth/div/mat-dialog-content/div[2]/form/mat-form-field[2]/div/div[1]/div/input')
-        email.send_keys('CPF para login')
-        senha.send_keys('Senha para login')
-        logar = self.chrome.find_element(By.XPATH, '/html/body/div/div[2]/div/mat-dialog-container/app-modal-auth/div/mat-dialog-content/div[2]/form/button')
+        email = self.chrome.find_element(By.CSS_SELECTOR, '#mat-input-0')
+        senha = self.chrome.find_element(By.CSS_SELECTOR, '#password')
+        email.send_keys('LOGIN')
+        senha.send_keys('SENHA')
+        logar = self.chrome.find_element(By.CSS_SELECTOR, '#mat-dialog-0 > app-modal-auth > div > mat-dialog-content > div.form-content.animated.ng-star-inserted > form > button')
         logar.click()
 
     def exit(self):
@@ -45,7 +46,7 @@ if __name__ == '__main__':
             sociorei = SocioRei()
             sociorei.open_site('https://www.sociorei.com/')
             sociorei.enter_account()
-            sleep(10)
+            sleep(5)
             sociorei.exit()
 
         except Exception as error: 
@@ -56,7 +57,7 @@ if __name__ == '__main__':
             sociorei = SocioRei()
             sociorei.open_site('https://www.sociorei.com/')
             sociorei.enter_account()
-            sleep(10)
+            sleep(3)
             sociorei.exit()
 
         else:
@@ -89,7 +90,7 @@ if __name__ == '__main__':
                     sociorei = SocioRei()
                     sociorei.open_site('https://www.sociorei.com/')
                     sociorei.enter_account()
-                    sleep(10)
+                    sleep(3)
                     sociorei.exit()
 
                 else:
